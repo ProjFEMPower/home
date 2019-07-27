546213a7271bfe9630921e2ccca97e248a2cf1be
Hilary Zen <hzen00@stuy.edu>
2019-07-27 19:20:32 -0400

    Added to homepage

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,17 @@ const Home = () => {
   return (
     <div id="home-body">
       <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet" />
-      <Header />
-      <h1 id="title">Project FEMPower</h1>
-      <p id="slogan">educate. empower. energize.</p>
+      <div id="title-block">
+        <Header />
+        <h1 id="title">Project FEMPower</h1>
+        <p id="slogan">energize. educate. empower.</p>
+      </div>
+      <div id="mission-block">
+        <h1>Our Mission</h1>
+        <p>Diversity in STEM has been a problem for decades, especially for women. Project FEMPower aims to use a unique, project-based curriculum to introduce girls to a wide variety of disciplines in STEM, provide support and mentorship, and ultimately encourage them to become leading scientists and engineers.</p>
+      </div>
+      <div id="about-block">
+      </div>
     </div>
   )
 }
